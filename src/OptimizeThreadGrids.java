0ed764f0d2cca6c71a03b5f27e88c2487b55d65c
Pavel <pasha.kuznetsov@gmail.com>
2010-03-04 18:59:52 +0600

    message

---
+++ src/OptimizeThreadGrids.java
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kuznetsov Pavel
+ * Date: 23.02.2010
+ * Time: 20:28:00
+ * To change this template use File | Settings | File Templates.
+ * This class working on optimizing  thread grids.
+ *
+ * It may be interesting to merge OptimizeThreadGrids & OptimizeThreadBlocks in single class. This class consist of methods
+ * for working with blocks & grids. And (partial) order of their processing.
+ */
+public class OptimizeThreadGrids
+{
+    
+}
---
+++ src/OptimizingThreadBlocks.java
@@ -0,0 +1,25 @@
+import jcuda.driver.CUarray;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kuznetsov Pavel
+ * Date: 23.02.2010
+ * Time: 20:06:04
+ * To change this template use File | Settings | File Templates.
+ * This class is contained methods for optimizing thread block sizes
+ * for the best thread execution
+ */
+public class OptimizingThreadBlocks
+{
+    OptimizingThreadBlocks()
+    {
+        jcuda.driver.CUdevice = 1;
+        int i=10;
+    }               gjdfh    sdfjh
+
+
+    public static void main(String[] args)
+    {
+
+    }
+}
